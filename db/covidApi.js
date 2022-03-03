26452ed2fb05318b39a456096a299f1734e4c3bf
Luis Solano <luissolano@iMac-de-Luis-2.local>
2022-03-03 09:04:26 -0500

    ok

--- db/covidApi.js
+++ db/covidApi.js
@@ -20,12 +20,12 @@ exports.paises =  ()=>{
 exports.paisesComoPoligonos = ()=>{
     const sql_sentence = `
         SELECT admin,
-        "Confirmed" as confirmados,
-        "Deaths" as muertes,
-        "Recovered" as recuperados,
+        confirmed as confirmados,
+        deaths as muertes,
+        recovered as recuperados,
         ST_ASGeoJSON(ST_Simplify(countries.geom,0.1,false)) AS geom 
         FROM cases_country,countries WHERE ST_CONTAINS(countries.geom,cases_country.geom) 
-        ORDER BY "Confirmed" DESC
+        ORDER BY "confirmed" DESC
     `
     return db.query(sql_sentence)
 }
